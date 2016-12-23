f76ec955f18b97fc1e77024907df211d46d8f795
Ghayyas Mubashir <ghayyasmubashir@gmail.com>
2016-12-24 01:37:35 +0500

    tesitng angular 2

--- simple-todos/client/main.js
+++ simple-todos/client/main.js
@@ -1,9 +1,21 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import todosList from '../imports/components/todoList/todoList';
- 
+import '../imports/startup/accounts-config.js';
+
 angular.module('simple-todos', [
   angularMeteor,
-  todosList.name
+  todosList.name,
+  'accounts.ui'
 ]);
+function onReady(){
+  angular.bootstrap(document,['simple-todos']);
+}
+
+if(Meteor.isCordova){
+  angular.element(document).on('deviceready',onReady);
+}
+else{
+  angular.element(document).ready(onReady);
+}
 console.log('todoList',todosList);
--- simple-todos/imports/components/todoList/todoList.js
+++ simple-todos/imports/components/todoList/todoList.js
@@ -1,19 +1,63 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import template from './todoList.html';
- 
+import {Task} from '../../api/task.js';
+import {Meteor} from 'meteor/meteor';
+
 class TodosListCtrl {
-  constructor() {
-    this.tasks = [{
-      text: 'This is task 1'
-    }, {
-      text: 'This is task 2'
-    }, {
-      text: 'This is task 3'
-    }];
+  constructor($scope) {
+    $scope.viewModel(this);
+    this.hideCompleted = false;
+    this.helpers({
+      task(){
+        const selector = {};
+        if(this.getReactively('hideCompleted')){
+          selector.checked = {
+            $ne: true
+          }
+        }
+        
+        
+        return Task.find({},{
+          
+          checked:{
+            $ne: true
+
+          },  
+          
+          sort:{
+            createdAt: -1
+          }
+        });
+      },
+      incrementCount(){
+        return Task.find({
+          checked:{
+            $ne:true
+          }
+        }).count();
+      },
+      currentUser(){
+        return Meteor.user()
+      }
+    });
+    // this.tasks = [{
+    //   text: 'This is task 1'
+    // }, {
+    //   text: 'This is task 2'
+    // }, {
+    //   text: 'This is task 3'
+    // }];
   }
   
   add(e){
+    if (!Meteor.userId()) {
+      window.alert('You need to login in first');
+          this.name = "";
+
+      // throw new Meteor.Error('not-authorized');
+    }
+    else{
     if(e == undefined){
       window.alert('Please Enter something');
     }
@@ -22,19 +66,30 @@ class TodosListCtrl {
       
     }
     else{
-    this.tasks.push({text:e});
+    // this.tasks.push({text:e});
+    Task.insert({
+      text:e,
+      createdAt: new Date,
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    })
     this.name = "";
   }
  }
+}
+
+
  delete(i){
    
   var d = window.confirm("Are you Sure You want to remove this ?");
    if(d){
-    var index = this.tasks.indexOf(i);
+     console.log('i',Task);
+      Task.remove(i._id);
+  //   var index = this.tasks.indexOf(i);
 
-    if (index > -1) {
-    this.tasks.splice(index, 1);
-  }
+  //   if (index > -1) {
+  //   this.tasks.splice(index, 1);
+  // }
 
  }
 }
@@ -43,12 +98,20 @@ edit(ind){
     var asked = window.prompt('Enter your Item',ind.text); 
   // console.log('ind', typeof(asked),asked);
   if(asked !== null){
-    for(var i=0;i < this.tasks.length;i++){
-    if(this.tasks[i].$$hashKey == ind.$$hashKey){
-        this.tasks[i].text= asked; 
-        break;
-    }
-  }
+   
+  Task.update(ind._id, {
+         $set: {
+             text: asked
+         },
+        });  
+    
+    
+   //   for(var i=0;i < this.tasks.length;i++){
+  //   if(Task._id == ind._id){
+  //       this.tasks[i].text= asked; 
+  //       break;
+  //   }
+  // }
  }
 }
 }
@@ -58,5 +121,5 @@ export default angular.module('todosList', [
 ])
   .component('todosList', {
     templateUrl: 'imports/components/todoList/todoList.html',
-    controller: [TodosListCtrl]
+    controller: ['$scope',TodosListCtrl]
   });
